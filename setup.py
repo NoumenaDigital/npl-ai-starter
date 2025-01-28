57697eb9a06c6622938b941b51a6e2561cd1353a
Ari von Nordenskjöld <ari@noumenadigital.com>
2025-01-28 10:52:33 +0100

    fix naming

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="npl-agent",
+    name="npl-ai-starter",
     version="0.1.0",
     package_dir={"": "python"},
     packages=find_packages(where="python"),
